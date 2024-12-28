09ebfbaf6db877ee0283b117a91b86d9c6f2795b
Nikolay Nikolov <nikolay@healee.com>
2024-12-28 12:56:35 +0200

    small update in AppointmentResource

--- app/Http/Resources/AppointmentResource.php
+++ app/Http/Resources/AppointmentResource.php
@@ -4,12 +4,13 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\Appointment;
 
 class AppointmentResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
-        return [
+        $data = [
             'id' => $this->id,
             'appointment_date' => $this->appointment_date,
             'client_name' => $this->client_name,
@@ -18,5 +19,21 @@ class AppointmentResource extends JsonResource
             'notification_method' => $this->notification_method->name,
             'user_id' => $this->user_id,
         ];
+        
+        if ($request->route()->getActionMethod() === 'show') {
+            $data['other_appointments'] = Appointment::where('egn', $this->egn)
+                ->where('id', '!=', $this->id)
+                ->where('appointment_date', '>', now())
+                ->get()
+                ->map(function ($appointment) {
+                    return [
+                        'id' => $appointment->id,
+                        'appointment_date' => $appointment->appointment_date,
+                        'description' => $appointment->description,
+                    ];
+                });
+        }
+
+        return $data;
     }
 }
